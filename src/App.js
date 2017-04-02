0946bc087e7d84ca303f2b5b8491c6db03434633
Josh Hawkins <jkhaccounts@gmail.com>
2017-04-02 11:42:24 -0500

    :sparkles: Add routing for specific episode or season selection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import './index.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import TitleBar from './components/TitleBar';
+import Main from './components/Main';
 
+import './index.css';
 import 'react-toolbox/lib/commons.scss';
 
+const Child = ({ match }) => (
+  <Main season={match.params.season} episode={match.params.episode}/>
+);
 
-import TitleBar from './components/TitleBar.js';
-import Main from './components/Main.js';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TitleBar />
-        <Main />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <Router>
+    <div className="App">
+      <TitleBar />
+      <Route exact path="/" component={Child} />
+      <Route exact path="/:season" component={Child} />
+      <Route exact path="/:season/:episode" component={Child} />
+    </div>
+  </Router>
+);
--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,44 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Button } from 'react-toolbox/lib/button';
 
-function commaSeparatedItems (item, index) {
+function commaSeparatedItems(item, index) {
   if (index === 0) {
-    return (
-      <span key={item}>{item}</span>
-    );
+    return <span key={item}>{item}</span>;
   } else {
-    return (
-      <span key={item}>, {item}</span>
-    );
+    return <span key={item}>, {item}</span>;
   }
 }
 
-class Episode extends Component {
-  render() {
-    return (
-      <div className="episode">
-        <label>Watch</label>
-        <h3>{this.props.episode.title}</h3>
-        <h5 className="dark">Season: {this.props.episode.season} Episode: {this.props.episode.episode}</h5>
+export default ({ episode }) => (
+  <div className="episode">
+    <label>Watch</label>
+    <h3>{episode.title}</h3>
+    <h5 className="dark">
+      Season: {episode.season} Episode: {episode.episode}
+    </h5>
 
-        <p className="description">{this.props.episode.description}</p>
+    <p className="description">{episode.description}</p>
 
-        <p><b>Lead characters: </b><span className="dark">{this.props.episode.lead.map(commaSeparatedItems)}</span></p>
-        {this.props.episode.guest ? <p><b>Guest characters: </b> <span className="dark">{this.props.episode.guest.map(commaSeparatedItems)}</span></p> : undefined}
-        <p><b>Written by: </b><span className="dark">{this.props.episode.writers.map(commaSeparatedItems)}</span></p>
+    <p>
+      <b>Lead characters: </b>
+      <span className="dark">{episode.lead.map(commaSeparatedItems)}</span>
+    </p>
+    {episode.guest
+      ? <p>
+          <b>Guest characters: </b>
+          {' '}
+          <span className="dark">{episode.guest.map(commaSeparatedItems)}</span>
+        </p>
+      : undefined}
+    <p>
+      <b>Written by: </b>
+      <span className="dark">{episode.writers.map(commaSeparatedItems)}</span>
+    </p>
 
-        <Button
-          raised
-          primary
-          target="_blank"
-          href={this.props.episode.link}
-          style={{marginBottom: 20, fontSize: '1.4rem'}}>
-          Watch Now on Netflix
-        </Button>
-      </div>
-    );
-  }
-}
-
-export default Episode;
+    <Button
+      raised
+      primary
+      target="_blank"
+      href={episode.link}
+      style={{ marginBottom: 20, fontSize: '1.4rem' }}
+    >
+      Watch Now on Netflix
+    </Button>
+  </div>
+);
