4a6d238d26ca4e1d5dd09d68b895eb4b4c0fa6f8
Josh Hawkins <jkhaccounts@gmail.com>
2017-12-25 15:04:59 -0600

    Add initial OpenGraph and Twitter previews

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "react-toolbox/lib/button";
+import { Helmet } from "react-helmet";
 
 function commaSeparatedItems(item, index) {
   if (index === 0) {
@@ -9,8 +10,36 @@ function commaSeparatedItems(item, index) {
   }
 }
 
+const home = "https://thegangpicksanepisode.com/";
+
 export default ({ episode }) => (
   <div className="episode">
+    <Helmet>
+      <meta
+        property="og:title"
+        content={`The Gang Picks an Episode: ${episode.title}`}
+      />
+      <meta property="og:image" content={`${home}/favicon.ico`} />
+      <meta property="og:descriptipn" content={episode.description} />
+      <meta
+        property="og:url"
+        content={`${home}/${episode.season}/${episode.episode}`}
+      />
+      <meta property="og:site_name" content="The Gang Picks an Episode" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary" />
+      <meta
+        name="twitter:title"
+        content={`The Gang Picks an Episode: ${episode.title}`}
+      />
+      <meta name="twitter:description" content={episode.description} />
+      <meta name="twitter:image" content={`${home}/favicon.ico`} />
+      <meta name="twitter:site" content="@hawkinjs" />
+      <meta name="twitter:site:id" content="@hawkinjs" />
+      <meta name="twitter:creator" content="@hawkinjs" />
+      <meta name="twitter:creator:id" content="@hawkinjs" />
+    </Helmet>
     <label>Watch</label>
     <h3>{episode.title}</h3>
     <h5 className="dark">
