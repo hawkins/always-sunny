d4ab642c53021f54ba5a96cf146c2b5718fe644b
Josh Hawkins <jkhaccounts@gmail.com>
2018-01-05 11:46:33 -0600

    Update react-autosuggest

--- razzle.config.js
+++ razzle.config.js
@@ -1,32 +1,27 @@
 module.exports = {
   modify: (config, { target, dev }, webpack) => {
-    // Note: This is ABSURDLY brittle and hacky,
-    // we're manually swapping out razzle's default CSS rule (index 4)
-    // with our own.
-    // How else can we override their CSS rules?
-    // Should be something like config.module.rules.push({
-    const cssRuleIndex = config.module.rules.findIndex(rule => {
-      if (!rule.test) return false;
-      return "a.css".match(rule.test);
-    });
-    config.module.rules[cssRuleIndex] = {
-      test: /\.css$/,
-      use: [
-        "isomorphic-style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            modules: true, // default is false
-            sourceMap: dev,
-            minimize: !dev,
-            importLoaders: 1,
-            localIdentName: "[name]--[local]--[hash:base64:8]"
-          }
-        },
-        "postcss-loader"
-      ]
-    };
-    // });
+    //   const cssRuleIndex = config.module.rules.findIndex(rule => {
+    //     if (!rule.test) return false;
+    //     return "a.css".match(rule.test);
+    //   });
+    //   config.module.rules[cssRuleIndex] = {
+    //     test: /\.css$/,
+    //     use: [
+    //       "isomorphic-style-loader",
+    //       {
+    //         loader: "css-loader",
+    //         options: {
+    //           modules: true, // default is false
+    //           sourceMap: dev,
+    //           minimize: !dev,
+    //           importLoaders: 1,
+    //           localIdentName: "[name]--[local]--[hash:base64:8]"
+    //         }
+    //       },
+    //       "postcss-loader"
+    //     ]
+    //   };
+    //   // });
     return config;
   }
 };
